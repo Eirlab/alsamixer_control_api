637334686a36c822ea566e9bd92b097747fef382
Sebastien Delpeuch <sebastien.delpeuch@enseirb-matmeca.fr>
2021-12-14 18:30:10 +0100

    ✨ feat(first): create a flask app

---
+++ app.py
@@ -0,0 +1,20 @@
+import os
+
+from flask import Flask
+
+app = Flask(__name__)
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_mapping(SECRET_KEY='dev')
+
+@app.route('/off')
+def off():
+    os.system("amixer set Capture 0%")
+    return "off"
+
+@app.route('/on')
+def on():
+    os.system("amixer set Capture 100%")
+    return "on"
+
